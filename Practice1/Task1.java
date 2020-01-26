2e7d890453e3a0743dbdbd918663586ad9cf874e
Yulia <yuliasincere@gmail.com>
2020-01-26 10:42:05 +0400

    Practice1_Task1

---
+++ Practice1/Task1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Task1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name ");
+        String s = sc.nextLine();
+        System.out.print("Hello "+s);
+    }
+}
