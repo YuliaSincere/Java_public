12e59efc8da8b38686bc01629d7cc7e0206f943b
Yulia <yuliasincere@gmail.com>
2020-01-29 16:30:17 +0400

    Practice1_Task4

--- Practice1/Task4.java
+++ Practice1/Task4.java
@@ -6,47 +6,51 @@ class Task4{
         int a = sc.nextInt();
         System.out.print("Enter second number ");
         int b = sc.nextInt();
-       /* System.out.print("Enter third number ");
-        int c = sc.nextInt();*/
+        System.out.print("Enter third number ");
+        int c = sc.nextInt();
         int n=0;
-        //searching for lowest commmon denomiator
-        if (a > b) {
-            if ((a % b) == 0) {
-                n = b;
-            }
-            else {
+        int h1 = a * b;
+        int h2 = a * c;
+        int h3 = c * b;
+        int h4 = a * b * c;
+        int aa = a;
+        int bb = b;
+        int cc = c;
+        //searching for greatest common divisor   part 1
+        while ((a != 0) && (b != 0)){
+            if (a > b) {
                 a = a % b;
-            }
-        }
-        else {
-            if ((b % a) == 0) {
-                n = a;
             }
             else {
                 b = b % a;
             }
         }
-        //part with third number (lcd)
-        /*if ( (a+b) > c) {
-            if (((a+b) % c) == 0) {
-                n = c;
-            }
-            else {
-                n = (a+b) % c;
-            }
-        }
-        else {
-            if ((c % (a+b)) == 0) {
-                n = (a+b);
+        n = a + b;
+        //searching for greatest common divisor  part 2
+        while ((n != 0) && (c != 0)){
+            if (n > c) {
+                n = n % c;
             }
             else {
                 c = c % n;
             }
-        }*/
+        }
+        int t = n + c;
+        int k = 0;
         //searching for lowest common multiply
-        n = a + b;
-        /*int k = (a * b) / n;*/
-        System.out.println("nod = "+n);
-        /*System.out.print("nok= "+k);*/
+        if ((h1 % t) % cc == 0) {
+            k = h1 / t;
+        }
+        else if ((h2 % t) % bb == 0) {
+            k = h2 / t;
+        }
+        else if ((h3 % t) % aa == 0) {
+            k = h3 / t;
+        }
+        else {
+            k = h4 / t;
+        }
+        System.out.println("nod = "+t);
+       System.out.print("nok= "+k);
     }
 }
