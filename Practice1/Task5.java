ad479f706a7f67fccbf0f10bbbc000e700d01729
Yulia <yuliasincere@gmail.com>
2020-01-29 20:00:33 +0400

    Practice1_Task5(first way)

---
+++ Practice1/Task5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter pls 15 adjectives ");
+        String adject1 = sc.nextLine();
+        String adject2 = sc.nextLine();
+        String adject3 = sc.nextLine();
+        String adject4 = sc.nextLine();
+        String adject5 = sc.nextLine();
+        String adject6 = sc.nextLine();
+        String adject7 = sc.nextLine();
+        String adject8 = sc.nextLine();
+        String adject9 = sc.nextLine();
+        String adject10 = sc.nextLine();
+        String adject11 = sc.nextLine();
+        String adject12 = sc.nextLine();
+        String adject13 = sc.nextLine();
+        String adject14 = sc.nextLine();
+        String adject15 = sc.nextLine();
+        System.out.println("Happy bday!");
+        System.out.println("You are so " + adject1 + " and " + adject2 + ". I wish you " + adject3 + " days and " + adject4 + " mood for ever. Oh, ive forgot to tell you? that you are " + adject5 + " ahah");
+        System.out.println("Be " + adject6 + " and " + adject7 + " and " + adject8 + " and " + adject9 + " I hope youll stay so " + adject10 + " as you now");
+        System.out.println("And stay " + adject11 + " and " + adject12 + " . You are the " + adject13 + " gift for our world. " + adject14 + " I love you, my " + adject15 + " star, ahah");
+    }
+}
