0b4007857bafad214b5d46923cb76743442e2705
Yulia <yuliasincere@gmail.com>
2020-04-18 06:15:49 +0400

    12.3

--- 12/Main.java
+++ 12/Main.java
@@ -1,23 +1,15 @@
 
 import java.util.Scanner;
 
+
 public class Main {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-        System.out.println("enter size");
-        int k = sc.nextInt();
-int[] f = new int[k];
-try { 
-for (int x = 0; x < k + 1; x++) {
-    f[x] = (int) x/(x-7);
-System.out.println("x="+x+", f[x]=" + f[x]);
-}
-}
-catch (ArithmeticException ae) {
-System.out.println("ДЕЛИШЬ НА 0");
-}
-catch (ArrayIndexOutOfBoundsException ai) {
-System.out.println("АУТ АВ БАУНД");
-}
-}
-}
+    String str = sc.nextLine(); 
+    String[] words = str.split("\\s"); //ограничитель - пробел
+           for(String subStr:words) {
+              System.out.println(subStr);  
+           }  
+        }
+     }
+
